8da467a73e5e4a4ff218d8445ab90fd606b1f164
Aahan kumar <116108954+anikeshxyz@users.noreply.github.com>
2025-09-13 12:18:01 +0530

    Time: 11 ms (90.26%) | Memory: 45.8 MB (43.31%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String frequencySort(String s) {
+       
+       Map<Character,Integer>freqMap =new HashMap<>();
+        for(char c :s.toCharArray()){
+            freqMap.put(c,freqMap.getOrDefault(c,0)+1);
+        }
+         PriorityQueue<Map.Entry<Character, Integer>> maxHeap =
+         new PriorityQueue<>((a, b) -> b.getValue() -a.getValue());
+
+         maxHeap.addAll(freqMap.entrySet());
+
+
+         StringBuilder sb =new StringBuilder();
+
+          while(!maxHeap.isEmpty()){
+            Map.Entry<Character,Integer> entry = maxHeap.poll();
+            char c =entry.getKey();
+            int Count  = entry.getValue();
+            for(int i =0; i<Count;i++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+
+
+       }
+    }
